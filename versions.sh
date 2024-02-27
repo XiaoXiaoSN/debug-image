2aae7722801ab1e418b8a90a0d83a26cd9ba56a2
XiaoXiaoSN <xiao.xiao@tenoz.tw>
2024-02-27 19:38:49 +0800

    feat: update version.sh

--- versions.sh
+++ versions.sh
@@ -4,7 +4,7 @@ set -e
 
 # helm latest
 helm=$(curl -s https://github.com/helm/helm/releases)
-helm_version=$(echo $helm\" | grep -oP '(?<=tag\/v)[0-9][^"]*' | grep -v \- | sort -Vr | head -1)
+helm_version=$(echo "$helm"\" | grep -oP '(?<=tag\/v)[0-9][^"]*' | grep -v \\- | sort -Vr | head -1)
 echo "⭐️ helm version is $helm_version"
 
 # kubectl latest
@@ -12,13 +12,13 @@ kubectl_version=$(curl -L -s https://dl.k8s.io/release/stable.txt)
 echo "⭐️ kubectl version is $kubectl_version"
 
 # kustomize latest
-kustomize_release=$(curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | /usr/bin/jq -r '.[].tag_name | select(contains("kustomize"))' \
+kustomize_release=$(curl -s https://api.github.com/repos/kubernetes-sigs/kustomize/releases | jq -r '.[].tag_name | select(contains("kustomize"))' \
   | sort -rV | head -n 1)
-kustomize_version=$(basename ${kustomize_release})
+kustomize_version=$(basename "${kustomize_release}")
 echo "⭐️ kustomize version is $kustomize_version"
 
 # kubeseal latest
-kubeseal_version=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/releases | /usr/bin/jq -r '.[].tag_name | select(startswith("v"))' \
+kubeseal_version=$(curl -s https://api.github.com/repos/bitnami-labs/sealed-secrets/releases | jq -r '.[].tag_name | select(startswith("v"))' \
   | sort -rV | head -n 1 | sed 's/v//')
 echo "⭐️ kubeseal version is $kubeseal_version"
 
@@ -29,10 +29,10 @@ if [ -n "$1" ]; then
     exit 127
   fi
 
-  sed -i "s/ARG HELM_VERSION=[0-9\.]*/ARG HELM_VERSION=$helm_version/" $1
-  sed -i "s/ARG KUBECTL_VERSION=v[0-9\.]*/ARG KUBECTL_VERSION=$kubectl_version/" $1
-  sed -i "s/ARG KUSTOMIZE_VERSION=v[0-9\.]*/ARG KUSTOMIZE_VERSION=$kustomize_version/" $1
-  sed -i "s/ARG KUBESEAL_VERSION=[0-9\.]*/ARG KUBESEAL_VERSION=$kubeseal_version/" $1
+  sed -i "s/ARG HELM_VERSION=[0-9\.]*/ARG HELM_VERSION=$helm_version/" "$1"
+  sed -i "s/ARG KUBECTL_VERSION=v[0-9\.]*/ARG KUBECTL_VERSION=$kubectl_version/" "$1"
+  sed -i "s/ARG KUSTOMIZE_VERSION=v[0-9\.]*/ARG KUSTOMIZE_VERSION=$kustomize_version/" "$1"
+  sed -i "s/ARG KUBESEAL_VERSION=[0-9\.]*/ARG KUBESEAL_VERSION=$kubeseal_version/" "$1"
 
   echo "✅ Versions replaced in $1"
 fi
